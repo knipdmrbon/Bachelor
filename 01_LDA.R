98edd59b04f0760070ea183ee67b11cd0d7c20a9
Pribahsnik Fabian 9500 sVAG <f_pribahsnikf@s-versicherung.co.at>
2018-08-05 19:27:45 +0200

    Change functions to handle column-names for LDA und QDA

--- 01_LDA.R
+++ 01_LDA.R
@@ -1,13 +1,17 @@
 ##############################################################################################################
-# This function takes two lists with the same number of elements. One list contains training and the other 
-# test data. The return value is a list of success rates.
+# This function takes two lists with the same number of elements and the columns one wants to use for the
+# LDA-algorithm. One list contains training and the other test data. The return value is a list of success rates.
 #
 # DT.train --> list of data.talbes where each data.table contains a training set.
 # DT.test  --> list of data.talbes where each data.table contains a test set.
+# columns ---> columns of the data-sets which should be used for training
 ##############################################################################################################
-learnLDA <- function(DT.train, DT.test, ...){
+learnLDA <- function(DT.train, DT.test, columns, ...){
+  
+  # generate a formula with the variables supplied by columns for the LDA - algorithm
+  LDA_formula <- as.formula(c("is_spam_flag ~", paste(columns, collapse = "+")))
   # apply the LDA-algorithm to the training-data --> use all variables as predictors
-  L.LDA_models <- lapply(DT.train, function(x){lda(is_spam_flag ~ ., data = x)})
+  L.LDA_models <- lapply(DT.train, function(x){lda(LDA_formula, data = x)})
   # use the trained LDA-models to predict the class-label for the test-data
   L.LDA_predict <- mapply(function(X,Y) {predict(X, Y)}, X = L.LDA_models, Y = DT.test, SIMPLIFY = FALSE)
   # generate a contingency table of the predicted and true class-labels
--- 02_QDA.R
+++ 02_QDA.R
@@ -1,13 +1,16 @@
 ##############################################################################################################
-# This function takes two lists with the same number of elements. One list contains training and the other 
-# test data. The return value is a list of success rates.
+# This function takes two lists with the same number of elements and the columns one wants to use for the
+# QDA-algorithm. One list contains training and the other test data. The return value is a list of success rates.
 #
 # DT.train --> list of data.talbes where each data.table contains a training set.
 # DT.test  --> list of data.talbes where each data.table contains a test set.
+# columns ---> columns of the data-sets which should be used for training
 ##############################################################################################################
-learnQDA <- function(DT.train, DT.test, ...){
+learnQDA <- function(DT.train, DT.test, columns, ...){
+  # generate a formula with the variables supplied by columns for the QDA - algorithm
+  QDA_formula <- as.formula(c("is_spam_flag ~", paste(columns, collapse = "+")))
   # apply the LDA-algorithm to the training-data --> use all variables as predictors
-  L.QDA_models <- lapply(DT.train, function(x){qda(is_spam_flag ~ ., data = x)})
+  L.QDA_models <- lapply(DT.train, function(x){qda(QDA_formula, data = x)})
   # use the trained LDA-models to predict the class-label for the test-data
   L.QDA_predict <- mapply(function(X,Y) {predict(X, Y)}, X = L.QDA_models, Y = DT.test, SIMPLIFY = FALSE)
   # generate a contingency table of the predicted and true class-labels
